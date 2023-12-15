60d3151b4b9dfdb6b32aec4aac8b5c64fa0fc65c
kiran <kiranjojan100@gmail.com>
2023-12-15 23:10:18 +0530

    Day 28: Add links, specify language, limit news, add subject, control category

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import requests
 
 # Import send_mail.py file and under that python file we \
-    # Import send_mail_user function
+# Import send_mail_user function
 from send_mail import send_mail_user
+
+topic = "apple"
+
 api = "e206eb1ef45147b5bdbe78ce94b2be5b"
-url = "https://newsapi.org/v2/everything?q=apple&from=2023-12-13&to=2023-12-13"\
-      "&sortBy=popularity&apiKey=e206eb1ef45147b5bdbe78ce94b2be5b"
+url = "https://newsapi.org/v2/everything" \
+      f"?q={topic}&from=2023-12-13&to=2023-12-13" \
+      "&sortBy=popularity&apiKey=e206eb1ef4514" \
+      "7b5bdbe78ce94b2be5b&language=en"
 
 # Make the request
 link_request = requests.get(url)
@@ -15,12 +20,12 @@ content = link_request.json()
 
 # Access articles, title and descriptions
 
-body = " "
-for articlas in content["articles"]:
+body = "Subject: Today's News " + "\n "
+for articlas in content["articles"][:20]:
     if articlas["title"] is not None:
-     body = body + articlas["title"] + "\n " + articlas["description"] + 2*"\n"
+        body = body + articlas["title"] + "\n " \
+               + articlas["description"] \
+               + "\n" + articlas["url"] + 2 * "\n"
 
 body = body.encode("utf-8")
 send_mail_user(message=body)
-
-
--- send_mail.py
+++ send_mail.py
@@ -1,15 +1,15 @@
 import smtplib
 import ssl
 
+
 def send_mail_user(message):
-    smpt_server = "smtp.gmail.com"
+    gmail_server = "smtp.gmail.com"
     port = 465
     user_name = "kiranjojan455@gmail.com"
-    user_password = "cvpwibtatkhoqgnw"
+    user_password = "gxvelsxemruixzsg"
 
     mail_receiver = "kiranjojan455@gmail.com"
     my_context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL(smpt_server,port,context=my_context) as server:
-        server.login(user_name,user_password)
-        server.sendmail(user_name,mail_receiver,message)
+    with smtplib.SMTP_SSL(gmail_server, port, context=my_context) as server:
+        server.login(user_name, user_password)
+        server.sendmail(user_name, mail_receiver, message)
